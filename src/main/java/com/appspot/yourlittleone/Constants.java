c47794daf09f220af62caa6b0bf1c159c72bd80b
Leon <yuelindu.pku@gmail.com>
2015-12-26 22:37:14 -0800

    conference works

--- src/main/java/com/appspot/yourlittleone/Constants.java
+++ src/main/java/com/appspot/yourlittleone/Constants.java
@@ -1,13 +1,17 @@
-package com.appspot.yourlittleone;
+package com.google.devrel.training.conference;
+
+import com.google.api.server.spi.Constant;
 
 /**
- * Contains the client IDs and scopes for allowed clients consuming the helloworld API.
+ * Contains the client IDs and scopes for allowed clients consuming the conference API.
  */
 public class Constants {
-  public static final String WEB_CLIENT_ID = "replace this with your web client ID";
-  public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
-  public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
-  public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
+    public static final String WEB_CLIENT_ID = "replace this with your web client id";
+    public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
+    public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
+    public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
+    public static final String EMAIL_SCOPE = Constant.API_EMAIL_SCOPE;
+    public static final String API_EXPLORER_CLIENT_ID = Constant.API_EXPLORER_CLIENT_ID;
 
-  public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
+    public static final String MEMCACHE_ANNOUNCEMENTS_KEY = "RECENT_ANNOUNCEMENTS";
 }
